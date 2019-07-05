90c7a365921beac74df26eb5cb004871126e60ab
楊晨志 <52515360+Yang-chen-chih@users.noreply.github.com>
2019-07-05 17:30:36 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,37 @@
 
 
 function start(e) {
-
+    var name = [
+        {
+                char: '楊    ',
+                big5: ' B7A8',
+                cns:  '  1-644E',
+                unicode: '  694A'
+        },{
+                char:'晨    ',
+                big5:'  B1E1',
+                cns:'  1-5B27',
+                unicode:'  6668'
+        },{
+                char:'志    ',
+                big5:'  A7D3',
+                cns:'  1-4A35',
+                unicode:'  5FD7 '
+        }
+    ];
+        var ele=document.getElementById('div1');
+        
+        for (var i=0;i<3;i++){
+        
+            ele.innerHTML += name[i].char;
+            ele.innerHTML +='   big5';
+            ele.innerHTML += name[i].big5;
+            ele.innerHTML +=' cns';
+            ele.innerHTML += name[i].cns;
+            ele.innerHTML +='    unicode';
+            ele.innerHTML += name[i].unicode;
+            ele.innerHTML +='<br>';
+        }
 }
 
 /*
